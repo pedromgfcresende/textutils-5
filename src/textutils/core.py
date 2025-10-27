bdcb3b0982de04fe2d8eaa182a3b582351971192
[Pedro Resende] <pedrom_resende@outlook.pt>
2025-10-27 16:36:55 +0100

    Initialize slugify function

---
+++ src/textutils/core.py
@@ -0,0 +1,11 @@
+def slugify(text):
+    """
+    This function should do the following
+    - Convert all characters to lowercase
+    - Remove final dots (periods) and replaces them with spaces
+    - Replace spaces and other non-alphanumeric characters with hyphens
+    - Remove leading, trailing, and repeated hyphens
+    - Raise a ValueError if input is not a string
+    """
+
+    raise NotImplementedx
