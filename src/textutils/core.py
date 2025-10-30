1bdeda04c4c4f29c3627d7bed020a7553d0671cd
[Pedro Resende] <pedrom_resende@outlook.pt>
2025-10-30 13:44:11 +0100

    Built the initial version of the function, rounding the result to the nearest integer

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,2 +1,23 @@
 def average_word_length(text):
-    raise NotImplemented
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  
+    lista = text.split()
+    number_of_words = len(lista)
+    if number_of_words == 0:
+        return 0
+    a = 0
+    for word in lista:
+        for letter in word:
+            if letter.lower() in alphabet or letter in numbers:
+                a +=1
+            else:
+                continue
+    
+    return round(a/number_of_words)
+
+
+                
+
+
+
+    
